2fb8e590ea765d467b6d19af547743fd0de14fe7
Piyush Jha <jhapiyush654@gmail.com>
2026-09-15 23:59:38 +0530

    Time: 5 ms (82.88%) | Memory: 45.5 MB (97.95%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev = 0;int temp = x;
+        while(x>0){
+            int rem = x%10;
+             rev = rev*10+rem;
+             x = x/10;
+        }
+        if(rev==temp){
+            return true;
+        }
+        else{
+          return false;
+        } 
+    }
+}
